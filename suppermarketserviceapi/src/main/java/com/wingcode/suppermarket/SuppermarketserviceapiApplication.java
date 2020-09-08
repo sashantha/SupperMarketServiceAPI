16475e76c5fcfc7b14dabe294f8abb14d0107597
Wingcod.MS <40849094+sashantha@users.noreply.github.com>
2020-09-09 01:21:20 +0530

    Service Api Project Created

---
+++ suppermarketserviceapi/src/main/java/com/wingcode/suppermarket/SuppermarketserviceapiApplication.java
@@ -0,0 +1,13 @@
+package com.wingcode.suppermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppermarketserviceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppermarketserviceapiApplication.class, args);
+	}
+
+}
---
+++ suppermarketserviceapi/src/test/java/com/wingcode/suppermarket/SuppermarketserviceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wingcode.suppermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppermarketserviceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
